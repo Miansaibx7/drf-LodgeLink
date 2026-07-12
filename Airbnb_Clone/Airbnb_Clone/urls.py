ac369edafba4b66b9539ab0bea0a65939096c0e1
MiansaibX7 <mianmwaqas77@gmail.com>
2026-07-12 10:09:38 +0500

    Initial commit

--- Airbnb_Clone/Airbnb_Clone/urls.py
+++ Airbnb_Clone/Airbnb_Clone/urls.py
@@ -1,8 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    include('account')
+    path("admin/", admin.site.urls),
+
+    # Auth app URLs
+    path("api/auth/", include("accounts.urls")),
+
+    # SimpleJWT refresh token endpoint
+    path("api/auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- Airbnb_Clone/accounts/urls.py
+++ Airbnb_Clone/accounts/urls.py
@@ -1,8 +1,40 @@
+# accounts/urls.py
+
 from django.urls import path
 
-
-
+from .views import (
+    RegisterView,
+    LoginView,
+    EmailOTPSendView,
+    EmailOTPVerifyView,
+    ResendEmailOTPView,
+    PasswordResetOTPSendView,
+    PasswordResetOTPVerifyView,
+    ChangePasswordView,
+    GoogleLoginView,
+    GitHubLoginView,
+    FacebookLoginView,
+    LinkedInLoginView,
+    LogoutView,
+)
 
 urlpatterns = [
-    path()
+#---------------------Register and Login Path--------------------------------------------------------
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(), name="login"),
+#----------------------- Email Path-------------------------------------------------------------------
+    path("otp/send/", EmailOTPSendView.as_view(), name="email_otp_send"),
+    path("otp/verify/", EmailOTPVerifyView.as_view(), name="email_otp_verify"),
+    path("otp/resend/", ResendEmailOTPView.as_view(), name="email_otp_resend"),
+#------------------------ Password Path---------------------------------------------------------------
+    path("password-reset/send/",PasswordResetOTPSendView.as_view(),name="password_reset_send",),
+    path("password-reset/verify/",PasswordResetOTPVerifyView.as_view(),name="password_reset_verify",),
+    path("change-password/", ChangePasswordView.as_view(), name="change_password"),
+#------------------------- OAuth Path--------------------------------------------------------------------
+    path("oauth/google/", GoogleLoginView.as_view(), name="google_login"),
+    path("oauth/github/", GitHubLoginView.as_view(), name="github_login"),
+    path("oauth/facebook/", FacebookLoginView.as_view(), name="facebook_login"),
+    path("oauth/linkedin/", LinkedInLoginView.as_view(), name="linkedin_login"),
+#-------------------------- Logout Path-------------------------------------------------------------------
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
