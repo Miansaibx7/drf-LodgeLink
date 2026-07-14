37c09734935044835688a10c94a3b9a58c6c2346
MiansaibX7 <mianmwaqas77@gmail.com>
2026-07-14 14:40:27 +0500

    Initial commit

--- Airbnb_Clone/accounts/models.py
+++ Airbnb_Clone/accounts/models.py
@@ -65,8 +65,6 @@ class User(AbstractBaseUser, PermissionsMixin, TimeStampedModel):
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     
-    date_joined = models.DateTimeField(auto_now_add=True)
-
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
     
