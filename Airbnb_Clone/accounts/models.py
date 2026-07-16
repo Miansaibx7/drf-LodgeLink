6a79079f331f727646ac98926ce6a8e28c22bac3
MiansaibX7 <mianmwaqas77@gmail.com>
2026-07-16 12:30:55 +0500

    Initial commit

--- Airbnb_Clone/accounts/models.py
+++ Airbnb_Clone/accounts/models.py
@@ -1,4 +1,4 @@
-from django.db import models, IntegrityError,transaction
+from django.db import models,transaction
 
 from django.contrib.auth.models import (AbstractBaseUser,BaseUserManager,PermissionsMixin,)
 from django.core.validators import RegexValidator, EmailValidator
