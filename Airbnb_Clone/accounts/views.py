7a77d5e8056596a2f87154f7ce7e77778fb57bff
MiansaibX7 <mianmwaqas77@gmail.com>
2026-07-19 11:27:50 +0500

    Initial commit

--- Airbnb_Clone/accounts/views.py
+++ Airbnb_Clone/accounts/views.py
@@ -6,6 +6,7 @@ from rest_framework.permissions import AllowAny,IsAuthenticated
 from rest_framework.throttling import AnonRateThrottle, UserRateThrottle # ratelimit
 
 from rest_framework.response import Response
+from rest_framework.request import Request
 from rest_framework import status, serializers
 
 from django.contrib.auth.models import update_last_login
