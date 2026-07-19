17cb91d1f811e234764cfc9c4aa1d635db5a1bff
MiansaibX7 <mianmwaqas77@gmail.com>
2026-07-19 20:58:23 +0500

    Initial commit

--- Airbnb_Clone/accounts/otp_logic/utils.py
+++ Airbnb_Clone/accounts/otp_logic/utils.py
@@ -12,7 +12,7 @@ from django.conf import settings
 
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
-from django.utils.html import strip_tags
+
 
 from rest_framework_simplejwt.tokens import RefreshToken
 
