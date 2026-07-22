915165dfcfcb81807290e07fc75dd020d37ea808
MiansaibX7 <mianmwaqas77@gmail.com>
2026-07-22 23:45:15 +0500

    Initial commit

--- Airbnb_Clone/Airbnb_Clone/urls.py
+++ Airbnb_Clone/Airbnb_Clone/urls.py
@@ -4,11 +4,12 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
+    # Django Admin Panel
     path("admin/", admin.site.urls),
 
-    # Auth app URLs
+    # Main Authentication Router (Includes Login, Register, OTP, OAuth, 2FA, Deletion)
     path("api/auth/", include("accounts.urls")),
 
-    # SimpleJWT refresh token endpoint
+    # SimpleJWT native endpoint for refreshing tokens
     path("api/auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
