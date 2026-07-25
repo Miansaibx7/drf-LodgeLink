126474975c9e1bbbace05168c97f9575bf7e3e74
MiansaibX7 <mianmwaqas77@gmail.com>
2026-07-25 23:44:41 +0500

    Initial commit

--- Airbnb_Clone/accounts/models.py
+++ Airbnb_Clone/accounts/models.py
@@ -67,7 +67,7 @@ class User(AbstractBaseUser, PermissionsMixin, TimeStampedModel):
     username = None
     # UUIDs prevent attackers from knowing how many users you have.
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    email = models.EmailField(unique=True, validators=[EmailValidator()]) # unique=True automatically creates a database index
+    email = models.EmailField(unique=True) # unique=True automatically creates a database index
     first_name = models.CharField(max_length=150, blank=True)
     last_name = models.CharField(max_length=150, blank=True)
     
