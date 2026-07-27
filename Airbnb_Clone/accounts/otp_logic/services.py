a5568780698945e4afcdb47a19d7524bb51afe1d
MiansaibX7 <mianmwaqas77@gmail.com>
2026-07-27 17:22:11 +0500

    Initial commit

--- Airbnb_Clone/accounts/otp_logic/services.py
+++ Airbnb_Clone/accounts/otp_logic/services.py
@@ -22,7 +22,7 @@ User = get_user_model()
 # ===================================== Helper Functions =====================================
 def _normalize_email(email: str) -> str:
     """Normalize email to lowercase and strip whitespace."""
-    return email.lower().strip()
+    return email.strip().casefold() # casefold() already performs lowercase conversion
 
 def _get_user_by_email(email: str) -> User: # type: ignore
     """ Retrieve a user by email.
