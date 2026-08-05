003663cc95ee2d938f0ca813dcd5ae874c0a7923
MiansaibX7 <mianmwaqas77@gmail.com>
2026-08-05 23:59:16 +0500

    Initial commit

--- Airbnb_Clone/accounts/urls.py
+++ Airbnb_Clone/accounts/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
-from .views import (
-    RegisterView, LoginView, LogoutView,
+from .views import (RegisterView, LoginView, LogoutView,
     EmailOTPSendView, EmailOTPVerifyView, ResendEmailOTPView,
     PasswordResetOTPSendView, PasswordResetOTPVerifyView, ChangePasswordView,
     GoogleLoginView, GitHubLoginView, FacebookLoginView, LinkedInLoginView
