f4741be756fa798102a330cd34e243e179841b7e
MiansaibX7 <mianmwaqas77@gmail.com>
2026-08-06 23:25:49 +0500

    Initial commit

--- Airbnb_Clone/accounts/urls.py
+++ Airbnb_Clone/accounts/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     path("oauth/facebook/", FacebookLoginView.as_view(), name="facebook_login"),
     path("oauth/linkedin/", LinkedInLoginView.as_view(), name="linkedin_login"),
 
-    # ------------------------- Two-Factor Auth ---------------------------
+    # ------------------------- Two-Factor Auth -------------------------------------------------
     path("2fa/setup/", TwoFactorSetupView.as_view(), name="2fa_setup"),
     path("2fa/verify/", TwoFactorVerifyView.as_view(), name="2fa_verify"),
     path("2fa/disable/", TwoFactorDisableView.as_view(), name="2fa_disable"),
