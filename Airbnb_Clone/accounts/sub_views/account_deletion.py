04db97eba010b53eadce03c4f5e76f9b5f94c87e
MiansaibX7 <mianmwaqas77@gmail.com>
2026-08-06 23:33:14 +0500

    Initial commit

--- Airbnb_Clone/accounts/sub_views/account_deletion.py
+++ Airbnb_Clone/accounts/sub_views/account_deletion.py
@@ -116,8 +116,7 @@ class AccountDeletionRequestView(APIView):
             reason=serializer.validated_data.get('reason', ''),confirm=serializer.validated_data['confirm'],
             request_data=request_data)
 
-        return Response({'success': True,
-            'message': 'Deletion request submitted. Your account will be deleted on {}.'.format(
+        return Response({'success': True,'message': 'Deletion request submitted. Your account will be deleted on {}.'.format(
                 deletion_request.scheduled_for.strftime('%Y-%m-%d %H:%M:%S')
             ),'request_id': deletion_request.id,'scheduled_for': deletion_request.scheduled_for
         }, status=status.HTTP_201_CREATED)
