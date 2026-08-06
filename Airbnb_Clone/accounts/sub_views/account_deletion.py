a1f69b5b65e0affaaa30a15c63cea3ec4a606ed3
MiansaibX7 <mianmwaqas77@gmail.com>
2026-08-06 23:32:01 +0500

    Initial commit

--- Airbnb_Clone/accounts/sub_views/account_deletion.py
+++ Airbnb_Clone/accounts/sub_views/account_deletion.py
@@ -72,6 +72,7 @@ class AccountDeletionService:
     def cancel_deletion_request(user: User, request_data: dict = None) -> None:
         """ Cancel a pending deletion request. Logs an ACCOUNT_DELETE. """
         request_obj = AccountDeletionRequest.objects.select_for_update().filter(user=user, completed=False, cancelled=False).first()
+
         if not request_obj:
             raise ServiceLayerError("No pending deletion request found.")
 
