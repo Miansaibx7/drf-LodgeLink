477b3eaaa1c211f564e971fca7298a55cef42640
mian muhammad waqas <mianmwaqas77@gmail.com>
2026-09-06 17:21:26 +0300

    Initial commit

--- Airbnb_Clone/listings/views.py
+++ Airbnb_Clone/listings/views.py
@@ -48,15 +48,9 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.parsers import FormParser, JSONParser, MultiPartParser
 from rest_framework.response import Response
 
-from .models import (
-    Amenity,
-    AmenityCategory,
-    Property,
-    PropertyCategory,
-    PropertyImage,
-)
-from .serializers import (
-    AmenityCategorySerializer,
+from .models import (Amenity,AmenityCategory,Property,PropertyCategory,PropertyImage,)
+
+from .serializers import (AmenityCategorySerializer,
     AmenitySerializer,
     PropertyAvailabilityBulkUpdateSerializer,
     PropertyAvailabilitySerializer,
