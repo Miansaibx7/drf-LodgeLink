4845eb53b8c5e04b48bd3a4327f3e05d0a519917
mian muhammad waqas <mianmwaqas77@gmail.com>
2026-09-13 20:49:23 +0300

    Initial commit

--- Airbnb_Clone/listings/views.py
+++ Airbnb_Clone/listings/views.py
@@ -55,9 +55,9 @@ from .serializers import (AmenityCategorySerializer,AmenitySerializer,PropertyAv
     PropertyDetailSerializer,PropertyImageBulkUploadSerializer,PropertyImageSerializer,PropertyListSerializer,PropertyMapSerializer)
 
 
-# ====================================================================
+# ==================================================================
 # PERMISSIONS
-# ====================================================================
+# ==================================================================
 class IsHostOrReadOnly(permissions.BasePermission):
     """
     Anyone (including anonymous users) can browse listings.
